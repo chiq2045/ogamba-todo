c34cdf2d6806ae2e1f0c4d91ac0dd789c6b37af9
chiq2045 <charles.oroko@outlook.com>
2023-01-05 05:04:44 -0800

    feat: complete appbar

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -16,56 +16,12 @@ export const AppBar = ({
             <h1 className='title text-xl'>To Do</h1>
           </Link>
         </div>
+        <div className='nav-item nav-btn'>
+          <span />
+          <span />
+          <span />
+        </div>
       </div>
-      {/* <Box> */}
-      {/*   <Flex align='center'> */}
-      {/*     <Spacer /> */}
-      {/*     <Box> */}
-      {/*       <InputGroup> */}
-      {/*         <InputLeftElement pointerEvents='none'> */}
-      {/*           <SearchIcon /> */}
-      {/*         </InputLeftElement> */}
-      {/*         <Input */}
-      {/*           variant='filled' */}
-      {/*           onChange={handleSearch} */}
-      {/*           value={searchValue} */}
-      {/*         /> */}
-      {/*       </InputGroup> */}
-      {/*     </Box> */}
-      {/*     <Spacer /> */}
-      {/*     <Box> */}
-      {/*       <Flex align='center'> */}
-      {/*         <Box> */}
-      {/*           <Menu> */}
-      {/*             <MenuButton as={Button} rightIcon={<ChevronDownIcon />}> */}
-      {/*               Sort By */}
-      {/*             </MenuButton> */}
-      {/*             <MenuList> */}
-      {/*               <MenuItem>Date Due</MenuItem> */}
-      {/*               <MenuItem>Recent</MenuItem> */}
-      {/*               <MenuItem>Alphabetical</MenuItem> */}
-      {/*             </MenuList> */}
-      {/*           </Menu> */}
-      {/*         </Box> */}
-      {/*         <Box> */}
-      {/*           <Spacer /> */}
-      {/*           <Menu> */}
-      {/*             <MenuButton as={Button} rightIcon={<ChevronDownIcon />}> */}
-      {/*               Sort By */}
-      {/*             </MenuButton> */}
-      {/*             <MenuList> */}
-      {/*               <MenuItem>Date Due</MenuItem> */}
-      {/*               <MenuItem>Recent</MenuItem> */}
-      {/*               <MenuItem>Alphabetical</MenuItem> */}
-      {/*             </MenuList> */}
-      {/*           </Menu> */}
-      {/*         </Box> */}
-      {/*         <Spacer /> */}
-      {/*       </Flex> */}
-      {/*     </Box> */}
-      {/*     <Spacer /> */}
-      {/*   </Flex> */}
-      {/* </Box> */}
     </nav>
   );
 };
