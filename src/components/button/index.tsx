4f328cf68e8178e73275e71b9914f6a8c6988389
chiq2045 <charles.oroko@outlook.com>
2023-01-30 23:39:33 -0800

    feat: add button props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,8 +1,25 @@
 import { HTMLAttributes, useRef } from 'react';
 import { AriaButtonProps, useButton } from 'react-aria';
 
+interface Props {
+  variant?: 'solid' | 'outline';
+  color?:
+    | 'transparent'
+    | 'light'
+    | 'dark'
+    | 'black'
+    | 'primary'
+    | 'link'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'danger';
+  size?: 'xs' | 'sm' | '' | 'lg' | 'xl';
+  animated?: boolean;
+  loading?: 'left' | 'right';
+}
 export const Button = (
-  props: AriaButtonProps & HTMLAttributes<HTMLButtonElement>
+  props: AriaButtonProps & HTMLAttributes<HTMLButtonElement> & Props
 ) => {
   const ref = useRef<HTMLButtonElement>(null);
   const { buttonProps } = useButton(props, ref);
