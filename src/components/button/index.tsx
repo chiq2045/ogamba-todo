657b9fd4a285a237fa989753e98ef31968763abb
chiq2045 <charles.oroko@outlook.com>
2023-03-23 17:31:45 -0700

    fix: restart work on app

--- src/components/button/index.tsx
+++
@@ -1,8 +0,0 @@
-import { HTMLAttributes } from 'react';
-
-export const Button = ({
-  children,
-  ...props
-}: HTMLAttributes<HTMLButtonElement>) => {
-  return <button {...props}>{children}</button>;
-};
--- src/components/tile/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './tile';
-export * from './tile-icon';
-export * from './tile-title';
-export * from './tile-buttons';
-export * from './tile-subtitle';
-export * from './tile-container';
